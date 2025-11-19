e15f0e2594df60c1e9e543b9be2dbfd25f432f1c
deleroLeo <96598166+deleroLeo@users.noreply.github.com>
2025-11-19 20:32:40 +0100

    modified:   src/components/cameraSection.jsx

--- src/components/cameraSection.jsx
+++ src/components/cameraSection.jsx
@@ -1,58 +1,70 @@
 "use client"
 
 import React from 'react'
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 
 const VidPlayer = ({url}) => {
+
+  const localVideoRef = useRef(null);
+  const remoteVideoRef = useRef(null);
+  const webrtcSendChannel = useRef(null);
+  const peerConnection = useRef(null);
+
   useEffect(() => {
-      const webrtc = new RTCPeerConnection({
+      peerConnection.current = new RTCPeerConnection({
       iceServers: [{
         urls: ['stun:stun.l.google.com:19302']
       }],
       sdpSemantics: 'unified-plan'})
 
-      webrtc.ontrack = function (event) {
-      console.log(event.streams.length + ' track is delivered')
-      return(
-        <video  autoplay controls srcObject = {event.streams[0]} style="width: 100%; height: auto;"/>
-      )
-    }
-    webrtc.addTransceiver('video', { direction: 'sendrecv' })
-    webrtc.addTransceiver('audio', {direction: 'sendrecv'})
-    webrtc.onnegotiationneeded = async function handleNegotiationNeeded () {
-      const offer = await webrtc.createOffer()
+      peerConnection.current.ontrack = event => {
+        remoteVideoRef.current.srcObject = event.streams[0]
+        console.log(event.streams.length + ' track is delivered')};
+      
+    
+      peerConnection.current.addTransceiver('video', { direction: 'sendrecv' })
+      peerConnection.current.addTransceiver('audio', {direction: 'sendrecv'})
+      peerConnection.current.onnegotiationneeded = async function handleNegotiationNeeded () {
+        const offer = await peerConnection.current.createOffer()
 
-      await webrtc.setLocalDescription(offer)
+        await peerConnection.current.setLocalDescription(offer)
 
-      fetch(url, {
-        method: 'POST',
-        body: new URLSearchParams({ data: btoa(webrtc.localDescription.sdp) })
-      })
-        .then(response => response.text())
-        .then(data => {
-          try {
-            webrtc.setRemoteDescription(
-              new RTCSessionDescription({ type: 'answer', sdp: atob(data) })
-            )
-          } catch (e) {
-            console.warn(e)
-          }
+        fetch(url, {
+          method: 'POST',
+          body: new URLSearchParams({ data: btoa(webrtc.localDescription.sdp) })
         })
-    }
+          .then(response => response.text())
+          .then(data => {
+            try {
+              peerConnection.current.setRemoteDescription(
+                new RTCSessionDescription({ type: 'answer', sdp: atob(data) })
+              )
+            } catch (e) {
+              console.warn(e)
+            }
+          })
+      }
 
-    const webrtcSendChannel = webrtc.createDataChannel('rtsptowebSendChannel')
-    webrtcSendChannel.onopen = (event) => {
-      console.log(`${webrtcSendChannel.label} has opened`)
-      webrtcSendChannel.send('ping')
+    webrtcSendChannel.current = peerConnection.current.createDataChannel('rtsptowebSendChannel')
+    webrtcSendChannel.current.onopen = (event) => {
+      console.log(`${webrtcSendChannel.current.label} has opened`)
+      webrtcSendChannel.current.send('ping')
     }
-    webrtcSendChannel.onclose = (_event) => {
-      console.log(`${webrtcSendChannel.label} has closed`)
+    webrtcSendChannel.current.onclose = (_event) => {
+      console.log(`${webrtcSendChannel.current.label} has closed`)
       VidPlayer(url)
     }
-    webrtcSendChannel.onmessage = event => console.log(event.data)
+    webrtcSendChannel.current.onmessage = event => console.log(event.data)
 
    }, []);
+   return (
+    <div>
+      
+        <video  ref = {remoteVideoRef} autoplay controls srcObject = {event.streams[0]} style={{width: '100%', height: 'auto'}}/>
+      
+    </div>
+   )
     
   }
 
